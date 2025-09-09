cac9bb7a55be330533d963fba56ef66e34918b75
wyattwong-git <wongwyattl@gmail.com>
2025-09-08 22:02:22 -0400

    Update Files

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -29,4 +29,29 @@ public class Multiples {
         }
         return count;
     }
+
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                count++;
+
+            } else if (divisibleBy3) {
+
+                count++;
+
+            } else if (divisibleBy5) {
+
+                count++;
+
+            }
+        }
+        return count;
+    }
 }
