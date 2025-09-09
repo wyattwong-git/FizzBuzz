a54cf3bf2882a96ad8e35ac962ce131c6b9b8961
wyattwong-git <wongwyattl@gmail.com>
2025-09-08 20:56:23 -0400

    Multiples Implementation

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,32 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                count++;
+
+            } else if (divisibleBy3) {
+
+                count++;
+
+            } else if (divisibleBy5) {
+
+                count++;
+
+            }
+        }
+        return count;
+    }
+}
